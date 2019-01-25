ad720587ea9350f8735d2f122d7f6dc12c201381
Daryl Johnston <daryl874@gmail.com>
2019-01-25 21:49:12 +0000

    update

--- src/Particle.ino
+++ src/Particle.ino
@@ -31,161 +31,136 @@
 
 // Include the display library
 #include <SH1106.h>
+#include <CellularHelper.h>
+#include "Particle.h"
 
-#define WiFi_Logo_width 60
-#define WiFi_Logo_height 36
-const char WiFi_Logo_bits[] PROGMEM = {
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF8,
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0xFF, 0x07, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0xE0, 0xFF, 0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF8, 0xFF,
-  0x7F, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFC, 0xFF, 0xFF, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0xFE, 0xFF, 0xFF, 0x01, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF,
-  0xFF, 0x03, 0x00, 0x00, 0x00, 0xFC, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00,
-  0x00, 0xFF, 0xFF, 0xFF, 0x07, 0xC0, 0x83, 0x01, 0x80, 0xFF, 0xFF, 0xFF,
-  0x01, 0x00, 0x07, 0x00, 0xC0, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x0C, 0x00,
-  0xC0, 0xFF, 0xFF, 0x7C, 0x00, 0x60, 0x0C, 0x00, 0xC0, 0x31, 0x46, 0x7C,
-  0xFC, 0x77, 0x08, 0x00, 0xE0, 0x23, 0xC6, 0x3C, 0xFC, 0x67, 0x18, 0x00,
-  0xE0, 0x23, 0xE4, 0x3F, 0x1C, 0x00, 0x18, 0x00, 0xE0, 0x23, 0x60, 0x3C,
-  0x1C, 0x70, 0x18, 0x00, 0xE0, 0x03, 0x60, 0x3C, 0x1C, 0x70, 0x18, 0x00,
-  0xE0, 0x07, 0x60, 0x3C, 0xFC, 0x73, 0x18, 0x00, 0xE0, 0x87, 0x70, 0x3C,
-  0xFC, 0x73, 0x18, 0x00, 0xE0, 0x87, 0x70, 0x3C, 0x1C, 0x70, 0x18, 0x00,
-  0xE0, 0x87, 0x70, 0x3C, 0x1C, 0x70, 0x18, 0x00, 0xE0, 0x8F, 0x71, 0x3C,
-  0x1C, 0x70, 0x18, 0x00, 0xC0, 0xFF, 0xFF, 0x3F, 0x00, 0x00, 0x08, 0x00,
-  0xC0, 0xFF, 0xFF, 0x1F, 0x00, 0x00, 0x0C, 0x00, 0x80, 0xFF, 0xFF, 0x1F,
-  0x00, 0x00, 0x06, 0x00, 0x80, 0xFF, 0xFF, 0x0F, 0x00, 0x00, 0x07, 0x00,
-  0x00, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0x01, 0x00, 0x00, 0xF8, 0xFF, 0xFF,
-  0xFF, 0x7F, 0x00, 0x00, 0x00, 0x00, 0xFE, 0xFF, 0xFF, 0x01, 0x00, 0x00,
-  0x00, 0x00, 0xFC, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF8, 0xFF,
-  0x7F, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE0, 0xFF, 0x1F, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0x80, 0xFF, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFC,
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  };
-
-
+STARTUP(cellular_credentials_set("TM", "", "", NULL));
 // Initialize the OLED display
+SYSTEM_MODE(SEMI_AUTOMATIC);
 SH1106 display(0x3c);
-
+//SYSTEM_MODE(MANUAL)
+//SYSTEM_THREAD(ENABLED)
 
 #define DEMO_DURATION 3000
 typedef void (*Demo)(void);
-
+FuelGauge fuel;
 int demoMode = 0;
 int counter = 1;
+int previousBattLevel = 0;
+int inPin = D4;
+int ledPin = D7;
+int alarmTriggered = 0;
+ String body = "Hey, this is your Particle Electron!";
+ CellularSignal sig;
+ bool alarmSent = false;
+ bool callFailed = false;
+void callStatus(int status);
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+void callStatus(int Status){
 
 
-  // Initialising the UI will init the display too.
-  display.init();
-
-  display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_10);
 
 }
+void connectingDisplay(){
 
-void drawFontFaceDemo() {
-    // Font Demo1
-    // create more fonts at http://oleddisplay.squix.ch/
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 0, "Hello world");
-    display.setFont(ArialMT_Plain_16);
-    display.drawString(0, 10, "Hello world");
-    display.setFont(ArialMT_Plain_24);
-    display.drawString(0, 26, "Hello world");
-}
-
-void drawTextFlowDemo() {
-    display.setFont(ArialMT_Plain_10);
-    display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.drawStringMaxWidth(0, 0, 128,
-      "Lorem ipsum\n dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore." );
-}
-
-void drawTextAlignmentDemo() {
-    // Text alignment demo
-  display.setFont(ArialMT_Plain_10);
-
-  // The coordinates define the left starting point of the text
+  // draw the current demo method
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0, 10, "Left aligned (0,10)");
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 0, "Connecting...");
+    display.display();
+    //SYSTEM_MODE(AUTOMATIC);
+}
+void displayString(String msg){
+  display.clear();
+  // draw the current demo method
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 0, msg);
+    display.display();
+}
+void displayLine2(String msg){
+  // draw the current demo method
+        display.setColor(BLACK);
+        display.fillRect(0, 16, 128, 24);
+        display.display();
 
-  // The coordinates define the center of the text
-  display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.drawString(64, 22, "Center aligned (64,22)");
+        display.setColor(WHITE);
+          display.display();
+        display.drawString(0, 16, msg);
+          display.display();
 
-  // The coordinates define the right end of the text
-  display.setTextAlignment(TEXT_ALIGN_RIGHT);
-  display.drawString(128, 33, "Right aligned (128,33)");
 }
 
-void drawRectDemo() {
-      // Draw a pixel at given position
-    for (int i = 0; i < 10; i++) {
-      display.setPixel(i, i);
-      display.setPixel(10 - i, i);
-    }
-    display.drawRect(12, 12, 20, 20);
-
-    // Fill the rectangle
-    display.fillRect(14, 14, 17, 17);
-
-    // Draw a line horizontally
-    display.drawHorizontalLine(0, 40, 20);
-
-    // Draw a line horizontally
-    display.drawVerticalLine(40, 0, 20);
-}
-
-void drawCircleDemo() {
-  for (int i=1; i < 8; i++) {
+void displayBattery(){
+  if ((int)fuel.getSoC() != previousBattLevel){
+        display.setColor(BLACK);
+    display.fillRect(50, 0,  78, 30);
+    display.display();
     display.setColor(WHITE);
-    display.drawCircle(32, 32, i*3);
-    if (i % 2 == 0) {
-      display.setColor(BLACK);
-    }
-    display.fillCircle(96, 32, 32 - i* 3);
+  previousBattLevel = (int)fuel.getSoC();
+  String message = "Batt: "+(String)(int)fuel.getSoC()+"%  ";
+  display.drawString(57, 0, message);
+    display.display();
   }
 }
 
-void drawProgressBarDemo() {
-  int progress = (counter / 5) % 100;
-  // draw the progress bar
-  display.drawProgressBar(0, 32, 120, 10, progress);
+void setup() {
 
-  // draw the percentage as String
-  display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.drawString(64, 15, String(progress) + "%");
+  //Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  // Initialising the UI will init the display too.
+  display.init();
+//Particle.function("callStatus", callStatus);
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);
+  pinMode(inPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  CellularSignal sig = Cellular.RSSI();
+Particle.keepAlive(6 * 60);
+
 }
 
-void drawImageDemo() {
-    // see http://blog.squix.org/2015/05/esp8266-nodemcu-how-to-create-xbm.html
-    // on how to create xbm files
-    display.drawXbm(34, 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
-}
+void displaySignal(){
+CellularHelperLocationResponse locResp = CellularHelper.getLocation();
+    CellularSignal sig = Cellular.RSSI();
+    display.setColor(BLACK);
+    display.fillRect(0, 35, 128, 24);
+    display.display();
+    display.setColor(WHITE);
+    CellularHelperRSSIQualResponse rssiQual = CellularHelper.getRSSIQual();
+    display.drawString(0, 40, (String)rssiQual.rssi);
+      display.display();
 
-Demo demos[] = {drawFontFaceDemo, drawTextFlowDemo, drawTextAlignmentDemo, drawRectDemo, drawCircleDemo, drawProgressBarDemo, drawImageDemo};
-int demoLength = (sizeof(demos) / sizeof(Demo));
-long timeSinceLastModeSwitch = 0;
+
+  }
+
+
 
 void loop() {
   // clear the display
-  display.clear();
-  // draw the current demo method
-  demos[demoMode]();
 
-  display.setTextAlignment(TEXT_ALIGN_RIGHT);
-  display.drawString(10, 128, String(millis()));
-  // write the buffer to the display
-  display.display();
-
-  if (millis() - timeSinceLastModeSwitch > DEMO_DURATION) {
-    demoMode = (demoMode + 1)  % demoLength;
-    timeSinceLastModeSwitch = millis();
-  }
-  counter++;
-  delay(10);
+displayBattery();
+displaySignal();
+alarmTriggered = digitalRead(inPin);
+    Serial.println(Particle.connected());
+ if(alarmTriggered){
+ digitalWrite(ledPin, HIGH);
+ Particle.connect();
+ if (Particle.connected() == 1){
+     displayLine2("Connected!");
+     if(!alarmSent|| callFailed){
+     Particle.publish("twilio_sms", body, PRIVATE);
+     alarmSent = true;
+   }
+ }else{
+    displayLine2("Connecting...");
+ }
+Serial.println("That's all!  You can restart or edit the code now.");
+}else{
+   digitalWrite(ledPin, LOW);
+  Particle.disconnect();
+  Cellular.off();
+  displayLine2("Ready!");
+}
 }
